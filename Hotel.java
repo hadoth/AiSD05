ab20352c6e5e276f6a3b6598c06984a9b2d84023
Karol <pokomeda.karol@gmail.com>
2017-04-19 07:03:43 +0200

    Define Hotel interface

---
+++ Hotel.java
@@ -0,0 +1,12 @@
+/**
+ * Created by Karol Pokomeda on 2017-04-13.
+ */
+public interface Hotel {
+    int getStars();
+    double getUserRating();
+    boolean hasPool();
+    boolean hasSpa();
+    boolean isConferenceReady();
+    double  getDistanceFrom(double xCoordinate, double yCoordinate);
+    double getPrice(int bedNumber);
+}
