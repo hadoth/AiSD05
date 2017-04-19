95276a27b797136cf7357749e5a8d2456aa80d72
Karol <pokomeda.karol@gmail.com>
2017-04-19 07:15:52 +0200

    Implement HotelBuilder class

--- HotelBuilder.java
+++ HotelBuilder.java
@@ -1,5 +1,16 @@
+import java.util.HashMap;
+
 /**
  * Created by Karol Pokomeda on 2017-04-19.
  */
 public class HotelBuilder {
+    private int starRate;
+    private int userRateSum = 0;
+    private int userRateNumber = 0;
+    private boolean hasPool = false;
+    private boolean hasSpa = false;
+    private boolean isConferenceReady = false;
+    private double xCoordinate;
+    private double yCoordinate;
+    private HashMap<Integer, Double> bedPrices = new HashMap<>();
 }
