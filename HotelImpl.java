c83387813e70add0583531008248018e1b0d31d9
Karol <pokomeda.karol@gmail.com>
2017-04-19 07:06:06 +0200

    Implement concrete class for Hotel Interface

---
+++ HotelImpl.java
@@ -0,0 +1,44 @@
+/**
+ * Created by Karol Pokomeda on 2017-04-19.
+ */
+public class HotelImpl implements Hotel {
+    @Override
+    public int getStars() {
+        return 0;
+    }
+
+    @Override
+    public double getUserRating() {
+        return 0;
+    }
+
+    @Override
+    public boolean hasPool() {
+        return false;
+    }
+
+    @Override
+    public boolean hasSpa() {
+        return false;
+    }
+
+    @Override
+    public boolean isConferenceReady() {
+        return false;
+    }
+
+    @Override
+    public double getDistanceFrom(double xCoordinate, double yCoordinate) {
+        return 0;
+    }
+
+    @Override
+    public double getPrice(int bedNumber) {
+        return 0;
+    }
+
+    @Override
+    public int compareTo(Hotel o) {
+        return 0;
+    }
+}
