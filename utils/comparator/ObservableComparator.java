a97cbb403917555d048c54eac7a3c0fe77b4655c
Karol <pokomeda.karol@gmail.com>
2017-04-20 07:49:35 +0200

    Add Observable interface to ObservableComparator

--- utils/comparator/ObservableComparator.java
+++ utils/comparator/ObservableComparator.java
@@ -1,17 +1,39 @@
 package utils.comparator;
 
+import utils.observer.Observable;
+import utils.observer.Observer;
+
+import java.util.ArrayList;
+
 /**
  * Created by Karol Pokomeda on 2017-04-20.
  */
-public class ObservableComparator<T> implements Comparator<T> {
+public class ObservableComparator<T> implements Comparator<T>, Observable {
     private Comparator<T> internalComparator;
+    private ArrayList<Observer> observers;
 
     public ObservableComparator(Comparator<T> internalComparator){
         this.internalComparator = internalComparator;
+        this.observers = new ArrayList<>();
     }
 
     @Override
     public int compare(T tLeft, T tRight) {
         return 0;
+    }
+
+    @Override
+    public boolean addObserver(Observer observer) {
+        return false;
+    }
+
+    @Override
+    public boolean deleteObserver(Observer observer) {
+        return false;
+    }
+
+    @Override
+    public void notifyObservers(SortingEvent event) {
+
     }
 }
