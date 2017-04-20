c8600f30fedd3be4e906460dc5577a57a305d5bb
Karol <pokomeda.karol@gmail.com>
2017-04-20 07:15:48 +0200

    Move files to specific packages

---
+++ utils/observer/Observable.java
@@ -0,0 +1,10 @@
+package utils.observer;
+
+/**
+ * Created by Karol Pokomeda on 2017-04-20.
+ */
+public interface Observable {
+    boolean addObserver(Observer observer);
+    boolean deleteObserver (Observer observer);
+    void notifyObservers(SortingEvent event);
+}
