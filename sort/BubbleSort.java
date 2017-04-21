abc13336532a76926841aa6e1c6e3d703c6632f3
Karol <pokomeda.karol@gmail.com>
2017-04-21 22:13:24 +0200

    Implement BubbleSort constructor and internal structure

--- sort/BubbleSort.java
+++ sort/BubbleSort.java
@@ -7,6 +7,12 @@ import utils.list.SwapList;
  * Created by Karol Pokomeda on 2017-04-21.
  */
 public class BubbleSort<T> implements ListSorter<T> {
+    private  SwapList<T> internalList;
+
+    public BubbleSort(SwapList<T> swapList){
+        this.internalList = swapList;
+    }
+
     @Override
     public MyList<T> sort(MyList<T> list) {
         return null;
