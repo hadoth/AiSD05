b806f7e4667415fed3a235d755b4bfc6f5775776
Karol <pokomeda.karol@gmail.com>
2017-04-21 21:34:16 +0200

    Refactor ObservableList

--- utils/list/ObservableList.java
+++ utils/list/ObservableList.java
@@ -7,19 +7,16 @@ import utils.observer.Observer;
 /**
  * Created by Karol Pokomeda on 2017-04-21.
  */
-public class ObservableList<T> implements Observable, ListInterface<T> {
-    private MyList<T> internalList;
+public class ObservableList<T> extends SwapList implements Observable {
     private MyList<Observer> observerList;
 
     public ObservableList(MyList<T> internalList){
-        this.internalList = internalList;
+        super(internalList);
         this.observerList = new MyList<>();
     }
 
     public void swap(int indexLeft, int indexRight){
-        T temp = this.internalList.get(indexLeft);
-        this.internalList.set(this.internalList.get(indexRight), indexLeft);
-        this.internalList.set(temp, indexRight);
+        super.swap(indexLeft, indexRight);
         this.notifyObservers(SortingEvent.SWAP);
     }
 
@@ -43,65 +40,5 @@ public class ObservableList<T> implements Observable, ListInterface<T> {
             observers.current().update(event);
             observers.next();
         }
-    }
-
-    @Override
-    public IteratorInterface<T> iterator() {
-        return this.internalList.iterator();
-    }
-
-    @Override
-    public int size() {
-        return this.internalList.size();
-    }
-
-    @Override
-    public void insert(T t, int index) throws IndexOutOfBoundsException {
-        this.internalList.insert(t, index);
-    }
-
-    @Override
-    public T get(int index) throws IndexOutOfBoundsException {
-        return this.internalList.get(index);
-    }
-
-    @Override
-    public T set(T t, int index) throws IndexOutOfBoundsException {
-        return this.internalList.set(t, index);
-    }
-
-    @Override
-    public void add(T t) {
-        this.internalList.add(t);
-    }
-
-    @Override
-    public T delete(T t) throws IndexOutOfBoundsException {
-        return this.internalList.delete(t);
-    }
-
-    @Override
-    public T delete(int index) throws IndexOutOfBoundsException {
-        return this.internalList.delete(index);
-    }
-
-    @Override
-    public boolean contains(T t) {
-        return this.internalList.contains(t);
-    }
-
-    @Override
-    public int indexOf(T t) {
-        return this.internalList.indexOf(t);
-    }
-
-    @Override
-    public boolean isEmpty() {
-        return this.internalList.isEmpty();
-    }
-
-    @Override
-    public void clear() {
-        this.internalList.clear();
     }
 }
