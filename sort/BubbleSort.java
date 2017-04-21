008922d05963bf08b4023e0baf28f338ca573e59
Karol <pokomeda.karol@gmail.com>
2017-04-21 21:57:47 +0200

    Implement BubbleSort class

---
+++ sort/BubbleSort.java
@@ -0,0 +1,14 @@
+package sort;
+
+import utils.list.MyList;
+import utils.list.SwapList;
+
+/**
+ * Created by Karol Pokomeda on 2017-04-21.
+ */
+public class BubbleSort<T> implements ListSorter<T> {
+    @Override
+    public MyList<T> sort(MyList<T> list) {
+        return null;
+    }
+}
