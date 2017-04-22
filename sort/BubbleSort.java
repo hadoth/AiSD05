631b44167708e05a11547df01081caab24ae39b4
Karol <pokomeda.karol@gmail.com>
2017-04-22 09:41:53 +0200

    Implement sort method in BubbleSort class

--- sort/BubbleSort.java
+++ sort/BubbleSort.java
@@ -1,20 +1,30 @@
 package sort;
 
-import utils.list.MyList;
+import utils.comparator.Comparator;
 import utils.list.SwapList;
 
 /**
  * Created by Karol Pokomeda on 2017-04-21.
  */
 public class BubbleSort<T> implements ListSorter<T> {
-    private  SwapList<T> internalList;
+    private Comparator<T> comparator;
 
-    public BubbleSort(SwapList<T> swapList){
-        this.internalList = swapList;
+    public BubbleSort(Comparator<T> comparator){
+        this.comparator = comparator;
     }
 
     @Override
-    public MyList<T> sort(MyList<T> list) {
-        return null;
+    public SwapList<T> sort(SwapList<T> list) {
+        boolean isSorted  = false;
+        int j = 1;
+        while (!isSorted){
+            isSorted = true;
+            for (int i = 0; i < list.size()-j; i++){
+                if (this.comparator.compare(list.get(i), list.get(i+1)) > 0) list.swap(i, i+1);
+                isSorted = false;
+            }
+            j++;
+        }
+        return list;
     }
 }
