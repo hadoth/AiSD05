4f3f5a0fc251a298078279ec29360cbe7854e15b
hadoth <valout13>
2017-04-22 22:58:45 +0200

    Implement HotelComparatorUserRating

---
+++ src/utils/comparator/hotel/HotelComparatorUserRating.java
@@ -0,0 +1,16 @@
+package utils.comparator.hotel;
+
+import hotel.Hotel;
+
+/**
+ * Created by Karol on 2017-04-22.
+ */
+public class HotelComparatorUserRating  extends HotelComparator {
+
+    @Override
+    public int compare(Hotel left, Hotel right) {
+        Double leftRating = left.getUserRating();
+        Double rightRating = right.getUserRating();
+        return leftRating.compareTo(rightRating);
+    }
+}
