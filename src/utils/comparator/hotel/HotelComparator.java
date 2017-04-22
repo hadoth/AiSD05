0830f8adc21b1669d2454ce5b7ab470162ab6e19
hadoth <valout13>
2017-04-22 22:30:36 +0200

    Implement HotelComparator

---
+++ src/utils/comparator/hotel/HotelComparator.java
@@ -0,0 +1,13 @@
+package utils.comparator.hotel;
+
+import hotel.Hotel;
+import utils.comparator.Comparator;
+
+/**
+ * Created by Karol on 2017-04-22.
+ */
+public abstract class HotelComparator<Hotel> implements Comparator<Hotel> {
+
+    @Override
+    public abstract int compare(Hotel left, Hotel right);
+}
